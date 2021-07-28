1874042f8447d2a87f93e579a7eeb72a2b0ce254
Eggyeggman <88085440+Eggyeggman@users.noreply.github.com>
2021-07-28 19:47:58 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the first variable: ')
+function setup(){
+  var b2 = createButton("click here to Swap");
+b2.mousePressed(switched)
+  }
+
+
+function draw()
+{
+}
+function switched(){
+  [a,b] = [b,a]
+console.log("the value of a"+ a)
+console.log("the value of b"+ b)}
